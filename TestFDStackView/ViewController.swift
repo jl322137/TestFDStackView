b0a31e745bcd0a14dd98c556ff616d5fcbde611a
江立 <322137@qq.com>
2015-10-10 12:08:55 +0800

    init

--- TestFDStackView/ViewController.swift
+++ TestFDStackView/ViewController.swift
@@ -10,16 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var stackView: UIView?
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func onPressedAddBtn(sender: UIButton) {
+        if let stackView = self.stackView {
+            stackView.removeFromSuperview()
+        }
+
+        self.stackView = nil
+
+        let nib = UINib(nibName: "NestStackView", bundle: nil)
+        if let view = nib.instantiateWithOwner(nil, options: nil).first as? UIView {
+            view.frame = CGRectMake(100, 100, 200, 200)
+            self.view.addSubview(view)
+            self.stackView = view
+        }
     }
 
+    @IBAction func onPressedRemoveBtn(sender: UIButton) {
+        if let stackView = self.stackView {
+            stackView.removeFromSuperview()
+        }
 
+        self.stackView = nil;
+    }
 }
 
